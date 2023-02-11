a0456cca1859851e10fe389e41ac3366b7386233
Давид <2vasyok2@gmail.com>
2023-02-11 15:51:39 +0300

    .env доработал

--- ya/ya/settings.py
+++ ya/ya/settings.py
@@ -10,27 +10,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv(find_dotenv())
-SECRET_KEY = os.environ.get("SECRET_KEY")
+env = environ.Env(
+    # Set casting, default value
+    DEBUG=(bool, True),
+    SECRET_KEY=(str, 'dummy-key'),
+    ALLOWED_HOSTS=(list, ['*']),
 
+)
+
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(',')
+DEBUG = env("DEBUG")
 
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 
 # Application definition
 
