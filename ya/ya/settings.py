11baf7730595a0830d79a389d375eb108da7a4d2
Давид <2vasyok2@gmail.com>
2023-02-11 15:17:14 +0300

    ALLOWED_HOSTS доработал

--- ya/ya/settings.py
+++ ya/ya/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("SECRET_KEY", True)
+DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(',')
 
 
 # Application definition
