e0ef7bafbc448e3822b3a1ff05f124f61b649952
Давид <2vasyok2@gmail.com>
2023-02-18 17:44:01 +0300

    Сделал issue: Тесты чайникового эндпоинта можно разделить #19

--- ya/homepage/tests.py
+++ ya/homepage/tests.py
@@ -1,6 +1,5 @@
 from http import HTTPStatus
 
-# from bs4 import BeautifulSoup
 from django.core.cache import cache
 from django.test import Client, TestCase
 
@@ -12,32 +11,18 @@ class StaticURLTests(TestCase):
         response = client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_coffee_endpoint(self):
+    def test_coffee_endpoint_status(self):
         cache.clear()
-        url = '/coffee/'
-        client = Client()
-        response = client.get(url)
-        # content = BeautifulSoup(response.content, 'html.parser')
-        # body_content = content.find('body').get_text()
-
-        self.assertEqual(response.content.decode('utf-8'), 'Я чайник')
-        self.assertEqual(response.status_code, 418)
-
-
-class MiddlewareReverseTextTests(TestCase):
-    def test_middleware_text_reverse(self):
-        """
-        Тестирую реверс в middleware текста из HttpResponse.
-        """
-        url = '/coffee/'
-        cache.clear()
-        client = Client()
-        for i in range(9):
-            client.get(url)
-
-        response = client.get(url)
-        body_content = response.content.decode('utf-8')
-        self.assertEqual(body_content, 'Я кинйач',
-                         msg=f'Неверный текст: {body_content} != Я кинйач')
+        response = Client().get('/coffee/')
         self.assertEqual(response.status_code, 418,
                          msg=f'Статус код: {response.status_code} != 418')
+
+    def test_coffee_endpoint_text(self):
+        cache.clear()
+        url = '/coffee/'
+        response = Client().get(url)
+        for i in range(9):
+            Client().get(url)
+        response_text = response.content.decode('utf-8')
+        self.assertEqual(response_text, 'Я чайник',
+                         msg=f'Неверный текст: {response_text} != Я кинйач')
