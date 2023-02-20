86cc1f7244a3d0d7fbdab81d0dd9cdf4f2d8232b
Давид <2vasyok2@gmail.com>
2023-02-20 20:01:56 +0300

    catalog models edited

--- ya/catalog/models.py
+++ ya/catalog/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from django.core.validators import RegexValidator
+from django.db import models
 
-# Create your models here.
+
+class CategoryBase(models.Model):
+    name = models.CharField('Название', max_length=150)
+    slug = models.SlugField(
+        verbose_name='Уникальный адрес',
+        unique=True,
+        max_length=200,
+        validators=[RegexValidator(r'^[\w-]+$')],
+    )
+    is_published = models.BooleanField(
+        'Опубликовано',
+        default=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        abstract = True
