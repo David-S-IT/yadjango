02fb03040f1782b4cb94f04fdbf7cd4d932f1688
Давид <2vasyok2@gmail.com>
2023-03-01 19:27:47 +0300

    1.4 ckeditor

--- ya/ya/settings.py
+++ ya/ya/settings.py
@@ -78,7 +78,9 @@ MIDDLEWARE = [
 MIDDLEWARE_CUSTOM_REVERSE_RU_TEXT_ENV = os.getenv(
     'MIDDLEWARE_CUSTOM_REVERSE_RU_TEXT', 'false'
 ).lower()
-MIDDLEWARE_CUSTOM_REVERSE_RU_TEXT = (MIDDLEWARE_CUSTOM_REVERSE_RU_TEXT_ENV in true_values)
+MIDDLEWARE_CUSTOM_REVERSE_RU_TEXT = (
+    MIDDLEWARE_CUSTOM_REVERSE_RU_TEXT_ENV in true_values
+)
 
 ROOT_URLCONF = 'ya.urls'
 
@@ -117,19 +119,19 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.'
-                'password_validation.UserAttributeSimilarityValidator',
+        'password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.'
-                'password_validation.MinimumLengthValidator',
+        'password_validation.MinimumLengthValidator',
     },
     {
         'NAME': 'django.contrib.auth.'
-                'password_validation.CommonPasswordValidator',
+        'password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.'
-                'password_validation.NumericPasswordValidator',
+        'password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -167,75 +169,139 @@ CKEDITOR_CONFIGS = {
     'default': {
         'skin': 'moono',
         # 'skin': 'office2013',
-        'toolbar_Basic': [
-            ['Source', '-', 'Bold', 'Italic']
-        ],
+        'toolbar_Basic': [['Source', '-', 'Bold', 'Italic']],
         'toolbar_YourCustomToolbarConfig': [
-            {'name': 'document',
-             'items': ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print',
-                       '-', 'Templates']},
-            {'name': 'clipboard',
-             'items': ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord',
-                       '-', 'Undo', 'Redo']},
-            {'name': 'editing',
-             'items': ['Find', 'Replace', '-', 'SelectAll']},
-            {'name': 'forms',
-             'items': ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea',
-                       'Select', 'Button', 'ImageButton',
-                       'HiddenField']},
+            {
+                'name': 'document',
+                'items': [
+                    'Source',
+                    '-',
+                    'Save',
+                    'NewPage',
+                    'Preview',
+                    'Print',
+                    '-',
+                    'Templates',
+                ],
+            },
+            {
+                'name': 'clipboard',
+                'items': [
+                    'Cut',
+                    'Copy',
+                    'Paste',
+                    'PasteText',
+                    'PasteFromWord',
+                    '-',
+                    'Undo',
+                    'Redo',
+                ],
+            },
+            {
+                'name': 'editing',
+                'items': ['Find', 'Replace', '-', 'SelectAll'],
+            },
+            {
+                'name': 'forms',
+                'items': [
+                    'Form',
+                    'Checkbox',
+                    'Radio',
+                    'TextField',
+                    'Textarea',
+                    'Select',
+                    'Button',
+                    'ImageButton',
+                    'HiddenField',
+                ],
+            },
             '/',
-            {'name': 'basicstyles',
-             'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript',
-                       'Superscript', '-', 'RemoveFormat']},
-            {'name': 'paragraph',
-             'items': ['NumberedList', 'BulletedList', '-', 'Outdent',
-                       'Indent', '-', 'Blockquote', 'CreateDiv', '-',
-                       'JustifyLeft', 'JustifyCenter', 'JustifyRight',
-                       'JustifyBlock', '-', 'BidiLtr', 'BidiRtl',
-                       'Language']},
+            {
+                'name': 'basicstyles',
+                'items': [
+                    'Bold',
+                    'Italic',
+                    'Underline',
+                    'Strike',
+                    'Subscript',
+                    'Superscript',
+                    '-',
+                    'RemoveFormat',
+                ],
+            },
+            {
+                'name': 'paragraph',
+                'items': [
+                    'NumberedList',
+                    'BulletedList',
+                    '-',
+                    'Outdent',
+                    'Indent',
+                    '-',
+                    'Blockquote',
+                    'CreateDiv',
+                    '-',
+                    'JustifyLeft',
+                    'JustifyCenter',
+                    'JustifyRight',
+                    'JustifyBlock',
+                    '-',
+                    'BidiLtr',
+                    'BidiRtl',
+                    'Language',
+                ],
+            },
             {'name': 'links', 'items': ['Link', 'Unlink', 'Anchor']},
-            {'name': 'insert',
-             'items': ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley',
-                       'SpecialChar', 'PageBreak', 'Iframe']},
+            {
+                'name': 'insert',
+                'items': [
+                    'Image',
+                    'Flash',
+                    'Table',
+                    'HorizontalRule',
+                    'Smiley',
+                    'SpecialChar',
+                    'PageBreak',
+                    'Iframe',
+                ],
+            },
             '/',
-            {'name': 'styles',
-             'items': ['Styles', 'Format', 'Font', 'FontSize']},
+            {
+                'name': 'styles',
+                'items': ['Styles', 'Format', 'Font', 'FontSize'],
+            },
             {'name': 'colors', 'items': ['TextColor', 'BGColor']},
             {'name': 'tools', 'items': ['Maximize', 'ShowBlocks']},
             {'name': 'about', 'items': ['About']},
             '/',  # put this to force next toolbar on new line
-            {'name': 'yourcustomtools', 'items': [
-                # put the name of your editor.ui.addButton here
-                'Preview',
-                'Maximize',
-
-            ]},
+            {
+                'name': 'yourcustomtools',
+                'items': [
+                    # put the name of your editor.ui.addButton here
+                    'Preview',
+                    'Maximize',
+                ],
+            },
         ],
         'toolbar': 'YourCustomToolbarConfig',
-        # put selected toolbar config here
-        # 'toolbarGroups': [{ 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] }],
-        # 'height': 291,
-        # 'width': '100%',
-        # 'filebrowserWindowHeight': 725,
-        # 'filebrowserWindowWidth': 940,
-        # 'toolbarCanCollapse': True,
-        # 'mathJaxLib': '//cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML',
         'tabSpaces': 4,
-        'extraPlugins': ','.join([
-            'uploadimage',  # the upload image feature
-            # your extra plugins here
-            'div',
-            'autolink',
-            'autoembed',
-            'embedsemantic',
-            'autogrow',
-            # 'devtools',
-            'widget',
-            'lineutils',
-            'clipboard',
-            'dialog',
-            'dialogui',
-            'elementspath'
-        ]),
+        'extraPlugins': ','.join(
+            [
+                'uploadimage',  # the upload image feature
+                # your extra plugins here
+                'div',
+                'autolink',
+                'autoembed',
+                'embedsemantic',
+                'autogrow',
+                # 'devtools',
+                'widget',
+                'lineutils',
+                'clipboard',
+                'dialog',
+                'dialogui',
+                'elementspath',
+            ]
+        ),
     }
 }
