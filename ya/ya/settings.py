8abe188eb7ca34c9fe29914348909c6adcfd9aa4
Давид <2vasyok2@gmail.com>
2023-03-02 16:13:08 +0300

    Выполнил issues

--- ya/ya/settings.py
+++ ya/ya/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'not_so_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG_ENV = os.getenv('DEBUG', 'false').lower()
+DEBUG_ENV = os.getenv('DEBUG', 'true').lower()
 DEBUG = DEBUG_ENV in true_values
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
