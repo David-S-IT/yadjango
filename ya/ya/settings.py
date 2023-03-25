e7f3e9dabe4fe0a0e494bcbb79ea098ef3612b78
Давид <2vasyok2@gmail.com>
2023-03-25 18:17:00 +0300

    ДЗ задача 1 - Done (fixed env)

--- ya/ya/settings.py
+++ ya/ya/settings.py
@@ -17,6 +17,7 @@ from django.contrib.messages import constants as message_constants
 import dotenv
 
 dotenv.load_dotenv()
+dotenv.load_dotenv(dotenv_path='../example.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
